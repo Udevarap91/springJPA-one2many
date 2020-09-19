3edb4b41d4472e5ac814093d274279625479ab48
Uday <udvearap@umich.edu>
2020-09-19 19:28:13 -0400

    first commit

---
+++ springJPA-one2many/src/main/java/com/javatechie/jpa/dto/OrderRequest.java
@@ -0,0 +1,19 @@
+package com.javatechie.jpa.dto;
+
+import com.javatechie.jpa.entity.Customer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class OrderRequest {
+
+    private Customer customer;
+}
